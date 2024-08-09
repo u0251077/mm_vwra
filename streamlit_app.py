186b1b1f1dbdeb5a8647b6b6d1f034442e54a387
Pei Ying <u0251077@gmail.com>
2024-08-09 10:43:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,21 +16,29 @@ def get_exchange_rate(base_currency, target_currency):
 def main():
     st.title("即時觀測")
 
+    # 常數
+    stock_count = 12   
+    already_money = 50000 
+    
     # 獲取 VWRA.L 股價
     vwra_stock_price = get_stock_price('VWRA.L')
 
     usd_twd_exchange_rate = get_exchange_rate('USD', 'TWD')  # 注意這只是範例
-    stock_count = 12    
+    
 
     
     st.write(f"VWRA.L 的股價: ${vwra_stock_price:.2f}")
     st.write(f"美元兌台幣匯率: {usd_twd_exchange_rate:.2f}")
-    st.write(f"累積數量: {stock_count:}")
-    stock_sum = stock_count*vwra_stock_price*usd_twd_exchange_rate
+    st.write("------------------")      
     
+    st.write(f"持有數量: {stock_count:}")
+    stock_sum = stock_count*vwra_stock_price*usd_twd_exchange_rate
+
+    st.write("------------------")      
     st.write(f"當前價值: {stock_sum:.2f}")
     st.write(f"台幣餘額: {195:.2f}")    
-    
+    st.write("------------------")  
+    st.write(f"已投資金額: {already_money:.2f}")
 
 if __name__ == "__main__":
     main()
